69daaf20e01e883b2461377669908168fd4ea93b
monsefCode <monsefetta4@gmail.com>
2023-06-14 17:29:38 +0100

    update next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   // experimental: {
   //   serverActions: true,
   // },
   images: {
+    loader: "custom",
     domains: [
       "images.pexels.com",
       "dummyimage.com",
