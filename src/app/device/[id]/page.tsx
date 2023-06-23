0b49872526795233ba04944da73ab7fefc5b7d40
monsefCode <monsefetta4@gmail.com>
2023-06-23 19:03:31 +0100

    update swiper

---
+++ src/app/device/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import { useRouter } from "next/navigation";
+
+const Device = ({ params }: any) => {
+  // const router = useRouter();
+  // console.log("====================================");
+  console.log(params);
+  // console.log("====================================");
+  return <div>Device details: {}</div>;
+};
+
+export default Device;
