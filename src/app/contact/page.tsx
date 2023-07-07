f9d04c9136e5ce2a3ded1dad8f66c265c90fa25a
monsefCode <monsefetta4@gmail.com>
2023-07-07 16:51:03 +0100

    added form validation

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { Fragment } from "react";
-import { Popover, Transition } from "@headlessui/react";
-import { XMarkIcon } from "@heroicons/react/24/outline";
-import Image from "next/image";
+import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 
 const navigation = [
   { name: "Work", href: "#" },
@@ -13,82 +13,36 @@ const navigation = [
 ];
 
 export default function Example() {
+  const validationSchema = z.object({
+    firstName: z.string().min(2, "Too Short!").max(50, "Too Long!"),
+    lastName: z.string().min(2, "Too Short!").max(50, "Too Long!"),
+    email: z.string().email("Invalid email"),
+    company: z.string().min(2, "Too Short!").max(50, "Too Long!"),
+    phone: z.string().min(2, "Too Short!").max(50, "Too Long!"),
+    message: z.string().min(2, "Too Short!").max(50, "Too Long!"),
+
+    howDidYouHearAboutUs: z.string().min(2, "Too Short!").max(50, "Too Long!"),
+  });
+
+  type Inputs = z.infer<typeof validationSchema>;
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm<Inputs>({
+    resolver: zodResolver(validationSchema),
+  });
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+    reset();
+  };
+
   return (
     <div className="bg-white">
-      {/* <Popover as="header" className="relative z-10">
-        <Transition
-          as={Fragment}
-          enter="duration-150 ease-out"
-          enterFrom="opacity-0 scale-95"
-          enterTo="opacity-100 scale-100"
-          leave="duration-100 ease-in"
-          leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
-        >
-          <Popover.Panel
-            focus
-            className="absolute inset-x-0 top-0 p-2 transition origin-top transform lg:hidden"
-          >
-            <div className="overflow-hidden bg-white rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
-              <div className="flex items-center justify-between px-5 pt-4">
-                <div>
-                  <Image
-                    height={32}
-                    width={32}
-                    className="w-auto h-8"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=purple&shade=600"
-                    alt=""
-                  />
-                </div>
-                <div className="-mr-2">
-                  <Popover.Button className="inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-grape-500">
-                    <span className="sr-only">Close menu</span>
-                    <XMarkIcon className="w-6 h-6" aria-hidden="true" />
-                  </Popover.Button>
-                </div>
-              </div>
-              <div className="pt-5 pb-6">
-                <div className="px-2 space-y-1">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="block px-3 py-2 text-base font-medium text-gray-900 rounded-md hover:bg-gray-50"
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
-                <div className="px-5 mt-6">
-                  <a
-                    href="#"
-                    className="block w-full px-4 py-2 font-medium text-center text-white border border-transparent rounded-md shadow bg-grape-500 hover:bg-grape-600"
-                  >
-                    Contact
-                  </a>
-                </div>
-              </div>
-            </div>
-          </Popover.Panel>
-        </Transition>
-      </Popover> */}
-
       <main>
-        {/* Header */}
-        {/* <div className="py-24 bg-gray-50 sm:py-32">
-          <div className="max-w-md pl-4 pr-8 mx-auto sm:max-w-lg sm:px-6 lg:max-w-7xl lg:px-8">
-            <h1 className="text-4xl font-bold leading-10 tracking-tight text-center text-gray-900 sm:text-5xl sm:leading-none lg:text-6xl">
-              Get in touch
-            </h1>
-            <p className="max-w-3xl mx-auto mt-6 text-xl leading-normal text-center text-gray-500">
-              Convallis feugiat et aliquet pellentesque dictum nisi, velit.
-              Egestas fermentum adipiscing risus quam ac consectetur mattis
-              turpis tristique.
-            </p>
-          </div>
-        </div> */}
-
-        {/* Contact Section */}
         <div className="relative bg-white">
           <div className="lg:absolute lg:inset-0">
             <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
@@ -98,7 +52,7 @@ export default function Example() {
                 className="object-cover w-full h-56 lg:absolute lg:h-full"
                 src="https://images.pexels.com/photos/3183148/pexels-photo-3183148.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                 alt="contacts"
-                unoptimized
+                loader={({ src }) => src}
               /> */}
               <div className="object-cover w-full h-56 lg:absolute lg:h-full contact"></div>
             </div>
@@ -115,8 +69,7 @@ export default function Example() {
                   email.
                 </p>
                 <form
-                  action="#"
-                  method="POST"
+                  onSubmit={handleSubmit(onSubmit)}
                   className="grid grid-cols-1 mt-9 gap-y-3 sm:grid-cols-2 sm:gap-x-8"
                 >
                   <div>
@@ -129,11 +82,16 @@ export default function Example() {
                     <div className="mt-1">
                       <input
                         type="text"
-                        name="first-name"
+                        {...register("firstName", { required: true })}
                         id="first-name"
                         autoComplete="given-name"
                         className="block w-full border-gray-300 rounded-md shadow-sm focus:border-grape-500 focus:ring-grape-500 sm:text-sm"
                       />
+                      {errors.firstName && (
+                        <span className="text-xs font-semibold text-red-500">
+                          {errors.firstName.message}
+                        </span>
+                      )}
                     </div>
                   </div>
                   <div>
@@ -146,11 +104,16 @@ export default function Example() {
                     <div className="mt-1">
                       <input
                         type="text"
-                        name="last-name"
+                        {...register("lastName", { required: true })}
                         id="last-name"
                         autoComplete="family-name"
                         className="block w-full border-gray-300 rounded-md shadow-sm focus:border-grape-500 focus:ring-grape-500 sm:text-sm"
                       />
+                      {errors.lastName && (
+                        <span className="text-xs font-semibold text-red-500">
+                          {errors.lastName.message}
+                        </span>
+                      )}
                     </div>
                   </div>
                   <div className="sm:col-span-2">
@@ -163,11 +126,16 @@ export default function Example() {
                     <div className="mt-1">
                       <input
                         id="email"
-                        name="email"
+                        {...register("email", { required: true })}
                         type="email"
                         autoComplete="email"
                         className="block w-full border-gray-300 rounded-md shadow-sm focus:border-grape-500 focus:ring-grape-500 sm:text-sm"
                       />
+                      {errors.email && (
+                        <span className="text-xs font-semibold text-red-500">
+                          {errors.email.message}
+                        </span>
+                      )}
                     </div>
                   </div>
                   <div className="sm:col-span-2">
@@ -188,7 +156,7 @@ export default function Example() {
                     <div className="mt-1">
                       <input
                         type="text"
-                        name="company"
+                        {...register("company")}
                         id="company"
                         autoComplete="organization"
                         className="block w-full border-gray-300 rounded-md shadow-sm focus:border-grape-500 focus:ring-grape-500 sm:text-sm"
@@ -213,7 +181,7 @@ export default function Example() {
                     <div className="mt-1">
                       <input
                         type="text"
-                        name="phone"
+                        {...register("phone")}
                         id="phone"
                         autoComplete="tel"
                         aria-describedby="phone-description"
@@ -239,12 +207,17 @@ export default function Example() {
                     <div className="mt-1">
                       <textarea
                         id="how-can-we-help"
-                        name="how-can-we-help"
+                        {...register("message", { required: true })}
                         aria-describedby="how-can-we-help-description"
                         rows={4}
                         className="block w-full border-gray-300 rounded-md shadow-sm focus:border-grape-500 focus:ring-grape-500 sm:text-sm"
                         defaultValue={""}
                       />
+                      {errors.message && (
+                        <span className="text-xs font-semibold text-red-500">
+                          {errors.message.message}
+                        </span>
+                      )}
                     </div>
                   </div>
                   <div className="sm:col-span-2">
@@ -257,7 +230,7 @@ export default function Example() {
                     <div className="mt-1">
                       <input
                         type="text"
-                        name="how-did-you-hear-about-us"
+                        {...register("howDidYouHearAboutUs")}
                         id="how-did-you-hear-about-us"
                         className="block w-full border-gray-300 rounded-md shadow-sm focus:border-grape-500 focus:ring-grape-500 sm:text-sm"
                       />
@@ -268,7 +241,30 @@ export default function Example() {
                       type="submit"
                       className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md shadow-sm bg-grape-600 hover:bg-grape-700 focus:outline-none focus:ring-2 focus:ring-grape-500 focus:ring-offset-2 bg-primary"
                     >
-                      Submit
+                      {isSubmitting ? (
+                        <svg
+                          className="w-5 h-5 mr-3 -ml-1 text-white animate-spin"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                        >
+                          <circle
+                            className="opacity-25"
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            stroke="currentColor"
+                            strokeWidth="4"
+                          />
+                          <path
+                            className="opacity-75"
+                            fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+                          />
+                        </svg>
+                      ) : (
+                        "Submit"
+                      )}
                     </button>
                   </div>
                 </form>
