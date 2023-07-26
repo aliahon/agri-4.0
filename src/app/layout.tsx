adcea5ce98cb8e9d25b273317724ecc069a10c96
monsefCode <monsefetta4@gmail.com>
2023-07-26 18:57:19 +0100

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body className={poppins.className}>
+      <body className={`${poppins.className} container max-w-3xl`}>
         <NavbarProvider>
           <Navbar />
           {children}
