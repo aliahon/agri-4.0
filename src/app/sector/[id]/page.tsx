6aed8dc7a2473399ece67c52f3961748a2825be7
Nohaila EL IDRISSI <nohaila09el@gmail.com>
2024-08-12 10:20:04 +0100

    some font fixing

--- src/app/sector/[id]/page.tsx
+++ src/app/sector/[id]/page.tsx
@@ -22,17 +22,17 @@ const Sector = ({ params }: any) => {
       {/* TOP SECTION */}
       <section className="flex flex-col md:flex-row justify-center w-full h-full bg-[#081B15] border-b-8 bg-gradient-to-r border-b-[#2DB273]">
         <div className="flex flex-col w-full h-full p-6 mt-10 md:pt-16 md:px-20">
-          <h1 className="max-w-sm text-xl font-bold text-white md:text-5xl drop-shadow-md">
+          <h1 className="max-w-sm text-5xl font-bold text-white md:text-5xl drop-shadow-md">
             {sector?.name}
           </h1>
-          <p className="max-w-lg mt-3 leading-tight text-justify text-gray-300 text-md">
+          <p className="max-w-lg mt-3 pr-0 leading-tight text-justify text-gray-300 text-md">
             {sector?.desc}
           </p>
         </div>
-        <div className="items-center justify-center w-[800px] h-[450px] md:flex">
+        <div className="items-center justify-center w-[1000px] h-[600px] md:flex">
           <Image
             src="/agric.png"
-            width={800}
+            width={1000}
             height={700}
             className="object-cover w-full h-full"
             alt="Agriculture de Précision"
@@ -60,6 +60,19 @@ const Sector = ({ params }: any) => {
                       setOpenTab(tab.id);
                     }}
                   >
+
+                    {/* les icons:
+                        - 1 : <svg class="h-5 w-5 text-green-800"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"    stroke-linejoin="round">  <circle cx="12" cy="12" r="10" />  <line x1="14.31" y1="8" x2="20.05" y2="17.94" />  <line x1="9.69" y1="8" x2="21.17" y2="8" />  <line x1="7.38" y1="12" x2="13.12" y2="2.06" />  <line x1="9.69" y1="16" x2="3.95" y2="6.06" />  <line x1="14.31" y1="16" x2="2.83" y2="16" />  <line x1="16.62" y1="12" x2="10.88" y2="21.94" /></svg>
+                        - 2 : 
+                        - 3 :
+                        - 4 :
+                        - 5 :
+                        - 6 :  <svg class="h-5 w-5 text-green-900"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="12" cy="12" r="9" />  <line x1="3.6" y1="9" x2="20.4" y2="9" />  <line x1="3.6" y1="15" x2="20.4" y2="15" />  <path d="M11.5 3a17 17 0 0 0 0 18" />  <path d="M12.5 3a17 17 0 0 1 0 18" /></svg>
+                        - 7 : 
+
+                    
+                    
+                    */}
                     <span
                       className={`w-14 h-10 rounded-full ${
                         openTab === tab.id ? "bg-primary" : "bg-[#2DB273]"
@@ -77,7 +90,7 @@ const Sector = ({ params }: any) => {
               </ul>
             </div>
             {/* SECOND SECTION */}
-            <section className="flex flex-col items-center justify-between px-4 py-10 md:px-24 md:flex-row">
+            <section className="flex flex-col items-center text-2xs justify-between px-4 py-10 md:px-24 md:flex-row">
               <div>
                 {/* {solutionsArr[openTab - 1].name !==
             "Vision et traitement d’image" ? (
@@ -94,24 +107,24 @@ const Sector = ({ params }: any) => {
             )} */}
                 {solutionsArr[openTab - 1].animation}
               </div>
-              <div className="flex-col justify-center ">
-                <h1 className="text-xl font-bold text-primary">
+              <div className="flex-col justify-center  lg:px-52">
+                <h1 className="text-xl font-bold pl-44 text-primary">
                   {solutionsArr[openTab - 1].name}
                 </h1>
-                <p className="max-w-3xl mt-3 text-sm text-gray-500">
+                <p className="max-w-3xl mt-3 text-base text-gray-500 pr-40 pl-44">
                   {solutionsArr[openTab - 1].desc}
                 </p>
-                <Link href={`/solutions/ap/${solutionsArr[openTab - 1].id}`}>
+                <Link href={`/solutions/ap/${solutionsArr[openTab - 1].id}`} className=" pl-44">
                   <button className="px-4 py-1.5 mt-4 border-2 text-primary rounded-xl border-secondary hover:bg-secondary hover:text-white transition duration-300 ease-in-out">
                     Discover the devices and solutions
                   </button>
                 </Link>
               </div>
             </section>
-            <section className="flex flex-col items-center justify-between px-4 py-10 space-y-5 bg-gray-100 md:px-24 md:flex-row md:space-y-0">
+            <section className="flex flex-col h:auto items-center justify-between px-4 py-10 space-y-5 bg-gray-100  lg:px-52 md:px-24 md:flex-row md:space-y-0">
               <div className="flex-col justify-center ">
                 <h1
-                  className="text-xl font-bold text-secondary"
+                  className="text-3xl font-bold text-secondary"
                   data-aos="fade-left"
                 >
                   Pourquoi l’agriculture de précision
@@ -145,13 +158,13 @@ const Sector = ({ params }: any) => {
                   src="/sectors/whyiot.png"
                   alt="why us"
                   className="object-cover h-[300px] rounded-xl w-full"
-                  width={1920}
-                  height={1080}
+                  width={537}
+                  height={381}
                   loader={({ src }) => src}
                 />
               </div>
             </section>
-            <section className="flex flex-col items-center justify-between px-6 py-10 space-y-5 md:px-24 md:flex-row md:space-y-0">
+            <section className="flex flex-col items-center justify-between px-6 py-10 space-y-5 md:px-24 md:flex-row md:space-y-0 lg:px-52">
               <div>
                 <Image
                   src="/sectors/ourGoals.png"
@@ -164,13 +177,13 @@ const Sector = ({ params }: any) => {
               </div>
               <div className="flex-col justify-center ">
                 <h1
-                  className="text-xl font-bold text-primary"
+                  className="text-3xl font-bold text-tertiary"
                   data-aos="fade-right"
                 >
                   Parmi nos missions à Agri 4.0 :
                 </h1>
                 <ul
-                  className="max-w-3xl mt-3 space-y-5 text-sm text-justify text-gray-700 list-disc marker:text-secondary marker:font-bold "
+                  className="max-w-3xl mt-3 space-y-5 text-sm text-justify text-gray-700 list-disc marker:text-tertiary marker:font-bold "
                   data-aos="fade-right"
                 >
                   <li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontSize: {
+        '2xs': '10px'
+      },
       colors: {
         primary: "#17352C",
         secondary: "#2DB273",
